98ee5e86a7b5aceeb755adb8ad4b8db0010bb97d
oscar ricardo vargas granados <voscar4955@gmail.com>
2016-03-24 00:16:46 -0600

    nuevo rumbo
    añadi un ejemplo de beatifull soup mañana trabajo en el primer method

--- Tarea I.py
+++ Tarea I.py
@@ -5,15 +5,5 @@ except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen
 
-html = urlopen("https://es.wikipedia.org/wiki/POST")
-#print(html.read())
-
-
-
-def recorer():
-    z= html.read()
-    x = 0 ;
-    while x< 15:
-        print(z[x]);
-        x =x+1
-        
+content = urlopen("https://www.quora.com/")
+print(content.read());
