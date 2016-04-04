34f3ddec4c87d1c65a42434401cf79e21863cfd4
oscar ricardo vargas granados <voscar4955@gmail.com>
2016-04-04 13:52:18 -0600

    nada especial

--- Tarea I.py
+++ Tarea I.py
@@ -1,9 +1,41 @@
+# For Python 3.0 and later
+import urllib.request
+import urllib.parse
+import re
+    
+"""
 try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
-except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
+    url = 'http://www.telcom.es/~gacias/naveg/iataicao.html'
+    headers = {}
+    headers['User-Agent']='Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'
+    req=urllib.request.Request(url,headers = headers)
+    resp = urllib.request.urlopen(req)
+    respData = resp.read()
 
-content = urlopen("https://www.quora.com/")
-print(content.read());
+    saveFile = open('withHeaders.txt', 'w')
+    saveFile.write(str(respData))
+    saveFile.close()
+
+except Exception as e:
+    print(str(e))
+
+
+
+etiqueta = '</strong><br>'
+
+
+where True:
+"""
+
+url = 'http://pythonprograming.net'
+values = {'s':'basics','submit':'search'}
+data= urllib.parse.rlencode(values)
+data = data.encode('utf-8')
+req = urllib.request.Request(url,data)
+resp = urllib.request.urlopen(req)
+respData = resp.read()
+
+print(respData)
+
+
+
